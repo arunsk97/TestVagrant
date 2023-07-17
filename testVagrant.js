c27d905cbaee70f95d04776b86d55e356e591b46
“arunsk” <santyancy@gmail.com>
2023-07-17 22:19:59 +0530

    changes.

--- testVagrant.js
+++ testVagrant.js
@@ -1,34 +1,67 @@
-function playlist(totalUsers) {
-    let curPlaylist = new Map();
-    //Map() is used for playlist creation because we need to add and delete in an order
+function playlist(totalUsers, totalPlaylistCap) {
+    curPlaylist = new Map(); //Map() is used for playlist creation because we need to add and delete in an order.
 
-    let setRecentlyPlayed = (user, song) => {
+    //setting playlist
+    this.setRecentlyPlayed = function (user, song) {
+        
+        if (curPlaylist.has(user)) {
+            const curUser = curPlaylist.get(user);
+            if(curUser.length >= totalPlaylistCap){
+                curUser.splice(0,1);
+                curUser.push(song);
+            }
+            else curUser.push(song);
+        } else {
+            curPlaylist.set(user, [song]);
+        }
         if (curPlaylist.size > totalUsers) {
             const firstUser = curPlaylist.keys().next().value;
             curPlaylist.delete(firstUser);
         }
-        if (curPlaylist.has(user)) {
-            const curUser = curPlaylist.get(user);
-            curUser.unshift(song);
-        } else {
-            curPlaylist.set(user, [song]);
-        }
     }
 
-    let printPlaylist = (user) =>{
-        console.log(`Recently Played list of ${user}:`);
-        const curUser = curPlaylist.get(user);
-        console.log(curUser.songs.join(", "))
+    //for getting individual playlists
+    this.getRecentlyPlayed = function (user) {
+        console.log(`Recently Played list of ${user}: ${curPlaylist.get(user) || []}`);
     }
 
-    let printFullPlaylist = () => {
-        console.log(`Recently Played list of ${user}:`);
+    //printing the whole playlist
+    this.printFullPlaylist = function () {
+        console.log(`Recently Played list :`);
         curPlaylist.forEach((songs, user) => {
-        console.log(`${user}: ${songs.join(", ")}`);
+            console.log(`${user}: ${songs.join(", ")}`);
         });
     }
-
-    let getRecentlyPlayed = (user) => {
-        return curPlaylist.get(user) || [];
-    }
 }
+const newPlaylist = new playlist(3, 3); // playlist is a constructor function.
+
+
+//creating new playlists
+newPlaylist.setRecentlyPlayed("Tom", "S1");
+newPlaylist.setRecentlyPlayed("Tom", "S2");
+newPlaylist.setRecentlyPlayed("Tom", "S3");
+newPlaylist.setRecentlyPlayed("Teena", "S1");
+newPlaylist.setRecentlyPlayed("Teena", "S2");
+newPlaylist.setRecentlyPlayed("Teena", "S3");
+newPlaylist.setRecentlyPlayed("Charlie", "S1");
+newPlaylist.setRecentlyPlayed("Charlie", "S2");
+newPlaylist.setRecentlyPlayed("Charlie", "S3");
+newPlaylist.printFullPlaylist();
+
+//for printing individual users playlist
+newPlaylist.getRecentlyPlayed("Tom");
+newPlaylist.getRecentlyPlayed("Teena");
+newPlaylist.getRecentlyPlayed("Charlie");
+//usecase when unknown user is called
+newPlaylist.getRecentlyPlayed("Dave"); 
+
+//condition when a new song is added after the song capacity is reached
+newPlaylist.setRecentlyPlayed("Tom", "S3");
+newPlaylist.setRecentlyPlayed("Teena", "S4");
+newPlaylist.setRecentlyPlayed("Charlie", "S5");
+newPlaylist.printFullPlaylist();
+
+//condition when a new user is added after the user capacity is reached
+newPlaylist.setRecentlyPlayed("Racheal", "S6")
+newPlaylist.printFullPlaylist();
+
